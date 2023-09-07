5fa843f332d8530ceaba4176c00aa79283cf7373
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-07 14:08:34 +0000

    [add]HashSetを追加

--- Collection.java
+++ Collection.java
@@ -12,19 +12,37 @@
 //   }
 // }
 
-import java.util.*;
+
+// import java.util.*;
+
+// public class Collection {
+//   public static void main(String[] args) {
+//     ArrayList<String> names = new ArrayList<String>();
+//     names.add("凪");
+//     names.add("朝香");
+//     names.add("菅原");
+//     names.add("有栖");
+//     Iterator<String> it = names.iterator();
+//     while(it.hasNext()) {
+//       String e = it.next();
+//       System.out.println(e);
+//     }
+//   }
+// }
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Collection {
   public static void main(String[] args) {
-    ArrayList<String> names = new ArrayList<String>();
-    names.add("凪");
-    names.add("朝香");
-    names.add("菅原");
-    names.add("有栖");
-    Iterator<String> it = names.iterator();
-    while(it.hasNext()) {
-      String e = it.next();
-      System.out.println(e);
-    }
+    Set<String> colors = new HashSet<String>();
+    colors.add("赤");
+    colors.add("青");
+    colors.add("黄");
+    colors.add("赤");
+    colors.add("緑");
+    System.out.println("色は" + colors.size() + "種類");
+
+
   }
 }
