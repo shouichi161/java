014d57aeef615156a6c09d96ada6ddb91609fd8a
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-08 14:50:26 +0000

    [add]HashMapを追加

--- Collection.java
+++ Collection.java
@@ -13,6 +13,7 @@
 // }
 
 
+
 // import java.util.*;
 
 // public class Collection {
@@ -29,6 +30,9 @@
 //     }
 //   }
 // }
+
+
+
 
 // import java.util.HashSet;
 // import java.util.Set;
@@ -48,18 +52,42 @@
 //   }
 // }
 
-import java.util.Set;
-import java.util.TreeSet;
+
+
+// import java.util.Set;
+// import java.util.TreeSet;
+
+// public class Collection {
+//   public static void main(String[] args) {
+//     Set<String> words = new TreeSet<String>();
+//     words.add("dog");
+//     words.add("cat");
+//     words.add("wolf");
+//     words.add("panda");
+//     for(String s : words) {
+//       System.out.print(s + "→");
+//     }
+//   }
+// }
+
+
+
+import java.util.*;
 
 public class Collection {
   public static void main(String[] args) {
-    Set<String> words = new TreeSet<String>();
-    words.add("dog");
-    words.add("cat");
-    words.add("wolf");
-    words.add("panda");
-    for(String s : words) {
-      System.out.print(s + "→");
+    Map<String, Integer> prefs = new HashMap<String, Integer>();
+    prefs.put("京都府",255);
+    prefs.put("東京都",1261);
+    prefs.put("熊本県",181);
+    int tokyo = prefs.get("東京都");
+    System.out.println("東京都の人口は, " + tokyo);
+    prefs.put("熊本県",182);
+    int kumamoto = prefs.get("熊本県") ;
+    System.out.println("熊本県の人口は, " + kumamoto);
+    for(String key : prefs.keySet()) {
+      int value = prefs.get(key);
+      System.out.println(key + "の人口は " + value);
     }
   }
 }
