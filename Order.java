517a958d5a3412bbff14effafc503469ab3513cc
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-09 14:08:44 +0000

    [update]Order.java

--- Order.java
+++ Order.java
@@ -32,5 +32,18 @@ public class Order {
     System.out.println("あなたの年齢を入力してください。");
     int age2 = new java.util.Scanner(System.in).nextInt();
     System.out.println("ようこそ" + age2 + "歳の" + name2 + "さん");
+
+
+    System.out.println("ようこそ占いの館へ");
+    System.out.println("あなたの名前を入力してください。");
+    String name3 = new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力してください。");
+    String ageString = new java.util.Scanner(System.in).nextLine();
+    int age3 = Integer.parseInt(ageString);
+    int fortune = new java.util.Random().nextInt(4);
+    fortune++;
+    System.out.println("占いの結果が出ました!");
+    System.out.println(age3 + "歳の" + name3 + "さん、あなたの運気番号は" + fortune + "です");
+    System.out.println("1:大吉　２:中吉　3:小吉　4:凶");
   }
 }
