ad98e4f0ce77c56895e5bab6ff84ad9271551f5d
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-09 13:50:05 +0000

    [update]Order.java

--- Order.java
+++ Order.java
@@ -16,14 +16,21 @@ public class Order {
     int b = 3;
     int m = Math.max(a,b);
     System.out.println("比較実験:" + a + "と" + b + "とで大きい方は" + m);
-    
+
     // String型をint型に変換する
     String age = "10";
     int n = Integer.parseInt(age);
     System.out.println("来年は、" + (n+1) + "歳になります");
-    
+
     // ランダムな数を生成
     int r = new java.util.Random().nextInt(90);
     System.out.println(r);
+
+    //キーボードの入力を受け付ける
+    System.out.println("あなたの名前を入力してください。");
+    String name2 = new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力してください。");
+    int age2 = new java.util.Scanner(System.in).nextInt();
+    System.out.println("ようこそ" + age2 + "歳の" + name2 + "さん");
   }
 }
