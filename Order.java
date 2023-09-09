f4cb829efa598b4f0576672d9c8d3ea4e4bdc6c6
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-09 09:59:24 +0000

    [add]Order.javaを追加

---
+++ Order.java
@@ -0,0 +1,29 @@
+public class Order {
+  public static void main(String[] args) {
+    String name = "すがわら";
+    // 改行あり
+    System.out.println("私の名前は");
+    System.out.println(name);
+    System.out.println("です");
+
+    // 改行なし
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です");
+
+    // 大きい方の数値を代入
+    int a = 5;
+    int b = 3;
+    int m = Math.max(a,b);
+    System.out.println("比較実験:" + a + "と" + b + "とで大きい方は" + m);
+    
+    // String型をint型に変換する
+    String age = "10";
+    int n = Integer.parseInt(age);
+    System.out.println("来年は、" + (n+1) + "歳になります");
+    
+    // ランダムな数を生成
+    int r = new java.util.Random().nextInt(90);
+    System.out.println(r);
+  }
+}
