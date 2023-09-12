72e238dcdf7e386f7d5ac72dd8b4fa2a2e7cd288
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-12 14:11:45 +0000

    [update]Array.java

--- Array.java
+++ Array.java
@@ -8,7 +8,7 @@ public class Array {
     int avg = sum / scores.length;
     System.out.println("合計点：" + sum);
     System.out.println("平均点：" + avg);
-    
+
     int count = 0;
     for (int i = 0; i < scores.length; i++) {
       if (scores[i] >= 50) {
@@ -16,5 +16,17 @@ public class Array {
       }
     }
     System.out.println("50点以上の科目数は" + count);
+
+
+    int[] seq = new int [10];
+
+    for (int j = 0; j < seq.length; j++) {
+      seq[j] = new java.util.Random().nextInt(4);
+    }
+
+    for (int j = 0; j < seq.length; j++) {
+      char[] base = {'A', 'T', 'G', 'C'};
+      System.out.print(base[seq[j]] + " ");
+    }
   }
 }
