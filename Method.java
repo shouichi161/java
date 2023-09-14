97c662aaab21f010955b843f106c00f41659fc20
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-14 13:08:37 +0000

    [add]Method.javaに複数の引数を追加

--- Method.java
+++ Method.java
@@ -6,10 +6,18 @@ public class Method {
     hello("朝香");
     hello("菅原");
     System.out.println("メソッドの呼び出しが終わりました");
+    //複数の引数
+    add(100 ,20);
+    add(200, 50);
   }
   public static void hello(String name) {
     System.out.println(name + "さん、こんにちは");
   }
-  
+
+  //複数の値を受け取るaddメソッド
+  public static void add(int x, int y) {
+    int ans = x + y;
+    System.out.println(x + "+" + y + "=" + ans);
+  }
 
 }
