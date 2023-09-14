57e63dfe6fc3494b63c21dd657115b9825dd967a
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-14 12:00:39 +0000

    [update]Array.java

--- Array.java
+++ Array.java
@@ -48,5 +48,27 @@ public class Array {
     int[][] scores4 ={{40, 50, 60},{80, 60, 70}};
     System.out.println(scores4.length);
     System.out.println(scores4[0].length);
+
+    int[] points = new int[4];
+    double[] weights = new double[5];
+    boolean[] answers = new boolean[3];
+    String[] names = new String[3];
+
+    int[] moneyList = {121902, 8302, 55100};
+    for (int i = 0; i < moneyList.length; i++) {
+      System.out.println(moneyList[i]);
+    }
+    for (int m : moneyList ) {
+      System.out.println(m);
+    }
+
+    int[] numbers = {3, 4, 9};
+    System.out.println("１桁の数字を入力してください");
+    int input = new java.util.Scanner(System.in).nextInt();
+    for (int n : numbers) {
+      if (n==input) {
+        System.out.println("あたり!");
+      }
+    }
   }
 }
