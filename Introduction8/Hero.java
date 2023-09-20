cfb225a3ff3310dc212b8233fdc40ec1f0b5785c
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-20 13:08:40 +0000

    [add]HeroクラスとMatangoクラスを作成

---
+++ Introduction8/Hero.java
@@ -0,0 +1,9 @@
+public class Hero {
+  String name;    //名前の宣言
+  int hp;         //HPの宣言
+  public void attack() {...}
+  public void sleep() {...}
+  public void sit(int sec) {...}
+  public void slip() {...}
+  public void run() {...}
+}
