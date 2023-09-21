9a753c2840bc7d539592ece7ac80d8a6e11d28f8
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-21 13:13:34 +0000

    [add]Mainに仮想世界に勇者を生み出す記述を追加

--- Introduction8/Hero.java
+++ Introduction8/Hero.java
@@ -1,14 +1,14 @@
 public class Hero {
   String name;    //名前の宣言
   int hp;         //HPの宣言
-  public void attack() {...}
+  // public void attack() {...}
   public void sleep() {
     this.hp = 100;
     System.out.println(this.name + "は、眠って回復した！");
   }
   public void sit(int sec) {
     this.hp += sec;
-    System.out.println(tihs.name + "は、 " + sec + "秒座った！");
+    System.out.println(this.name + "は、 " + sec + "秒座った！");
     System.out.println("HPが" + sec + "ポイント回復した");
   }
   public void slip() {
--- Introduction8/Main.java
+++ Introduction8/Main.java
@@ -1,7 +1,17 @@
 public class Main {
   public static void main(String[] args) {
     // (以下の内容をJavaで記述していく)
-    // 勇者よ、この仮想世界に生まれよ！
+    // 1.勇者を生成
+    Hero h = new Hero();
+    // 2.フィールドに初期値をセット
+    h.name = "ミナト";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました!");
+    // 3.勇者のメソッドを呼び出していく
+    h.sit(5);
+    h.slip();
+    h.sit(25);
+    h.run();
     // お化けキノコよ、この仮想世界に生まれよ！
     // 勇者よ、戦え！
     // お化けキノコよ、逃げろ！
