21d6aca42e22b57a7c48353e260fd061c8eb4416
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-26 14:11:27 +0000

    [add]本、コンピュータ、有形資産のクラスを作成

---
+++ Introduction11/Computer.java
@@ -0,0 +1,9 @@
+public class Computer extends TangibleAsset {
+  String makeName;
+  public computer(String name, int price, String color, String makeName) {
+    super(name, price, color;)
+    this.makeName = makeName;
+  }
+  public String getMakeName() {return this.makeName; }
+  }
+}
