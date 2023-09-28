2e9fc0493a27e319405e1a65340aba63ecfeb28a
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-28 13:57:18 +0000

    [add]12-7を追加

---
+++ Introduction12/12-7/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+  public static void main(String[] args) {
+    Y[] y3 = new Y[2];
+    y3[0] = new A();
+    y3[1] = new B();
+    for (Y y : y3) {
+      y.b();
+    }
+  }
+}
