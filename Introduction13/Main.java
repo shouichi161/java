d4a835d7eecc70b196c55285f3a87d9a3eb93737
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-09-30 12:40:54 +0000

    [add]setterメソッドに値の妥当性チェックを追加

--- Introduction13/Main.java
+++ Introduction13/Main.java
@@ -1,8 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    Hero h1= new Hero("ミナト");
-    
+    Hero h1 = new Hero("ミナト");
     King k = new King();
     k.talk(h1);
+    h1.setName("");
   }
 }
