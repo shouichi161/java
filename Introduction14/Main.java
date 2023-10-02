a5174a3a3943c583f039dba327b7c6f6d6d80628
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-02 13:00:54 +0000

    [add]Heroクラスにequalのオーバーライドを追加

--- Introduction14/Main.java
+++ Introduction14/Main.java
@@ -13,9 +13,17 @@ public class Main {
     p.printAnything(100);
     p.printAnything("Hello");
     p.printAnything(o3);
-    Hero h = new Hero();
-    h.name = "ミナト";
-    h.hp = 100;
-    System.out.println(h.toString());
+    Hero h1 = new Hero();
+    h1.name = "ミナト";
+    h1.hp = 100;
+    System.out.println(h1.toString());
+    Hero h2 = new Hero();
+    h2.name = "ミナト";
+    h2.hp = 100;
+    if (h1.equals(h2) == true) {
+      System.out.println("同じ内容です");
+    }  else {
+      System.out.println("違う内容です");
+    }
   }
 }
