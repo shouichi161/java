9d138371bb5c284177e47ba16ad72a9b27424417
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-05 12:16:36 +0000

    [add]StringBuilderを追加

--- Main.java
+++ Main.java
@@ -29,5 +29,12 @@ public class Main {
     String s6 = "Java programming";
     System.out.println("文字列s6の４文字目以降は" + s6.substring(3));
     System.out.println("文字列s6の1~8文字目は" + s6.substring(3,8));
+    
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 10; i++) {
+      sb.append("Java");
+    }
+    String s = sb.toString();
+    System.out.println(s);
   }
 }
