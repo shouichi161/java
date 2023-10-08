31d3de8290f09d895673f92f96b5a09b57c95038
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-08 11:40:40 +0000

    [add]Periodクラスを追加

--- Introductionq15/15-7~/Main.java
+++ Introductionq15/15-7~/Main.java
@@ -51,5 +51,18 @@ public class Main {
     if (now.isAfter(ldatep)) {
       System.out.println("1000日後は過去日付です");
     }
+    
+    LocalDate d1 = LocalDate.of(2020,1,1);
+    LocalDate d2 = LocalDate.of(2020,1,5);
+    
+    Period p1 = Period.ofDays(3);
+    Period p2 = Period.between(d1,d2);
+    
+    LocalDate d3 = d2.plus(p2);
+    System.out.println(d1);
+    System.out.println(d2);
+    System.out.println(p1);
+    System.out.println(p2);
+    System.out.println(d3);
   }
 }
