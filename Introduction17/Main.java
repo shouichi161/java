f5d394152b5e0224d8d5bca7944eb3e188556a61
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-13 13:49:23 +0000

    [add]catchを追加

--- Introduction17/Main.java
+++ Introduction17/Main.java
@@ -2,10 +2,20 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
+    FileWriter fw = null;
     try {
-      FileWriter fw = new FileWriter("data.txt");
+      fw = new FileWriter("data.txt");
+      fw.write("Hello!");
     } catch (IOException e) {
       System.out.println("エラーが発生しました。");
+    } finally {
+      if (fw != null){
+        try {
+          fw.close();
+        } catch (IOException e) {
+          ;
+        }
+      }
     }
   }
 }
