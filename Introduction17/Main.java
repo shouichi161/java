2b08d0bf4ee0f3b612c02bdd99235d19fb38e677
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-13 14:01:10 +0000

    [update]tryーwith-resources文を追加

--- Introduction17/Main.java
+++ Introduction17/Main.java
@@ -2,20 +2,11 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
-    FileWriter fw = null;
-    try {
-      fw = new FileWriter("data.txt");
+
+    try (FileWriter fw = new FileWriter("data.txt");) {
       fw.write("Hello!");
     } catch (IOException e) {
       System.out.println("エラーが発生しました。");
-    } finally {
-      if (fw != null){
-        try {
-          fw.close();
-        } catch (IOException e) {
-          ;
-        }
-      }
     }
   }
 }
