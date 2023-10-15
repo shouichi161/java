4a86cbc0e79d83a558bc0f080d9d8f48e2e2c8c4
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-15 10:23:08 +0000

    (add]例外を投げるを追加

--- Introduction17/Main.java
+++ Introduction17/Main.java
@@ -1,22 +1,30 @@
 import java.io.*;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    System.out.println("プログラムが起動しました。");
+    throw new IOException();
 
-    try (FileWriter fw = new FileWriter("data.txt");) {
-      fw.write("Hello!");
-    } catch (IOException e) {
-      System.out.println("エラーが発生しました。");
-    }
+    // try (FileWriter fw = new FileWriter("data.txt");) {
+    //   fw.write("Hello!");
+    // } catch (IOException e) {
+    //   System.out.println("エラーが発生しました。");
+    // }
 
-    String s = null;
-    try {
-    System.out.println(s.length());
-    } catch (NullPointerException e) {
-      System.out.println("NullPointerException 例外をcatchしました");
-      System.out.println("ーースタックトレース（ここから）ーー");
-      e.printStackTrace();
-      System.out.println("ーースタックトレース（ここまで）ーー");
-    }  
+    // String s = null;
+    // try {
+    // System.out.println(s.length());
+    // } catch (NullPointerException e) {
+    //   System.out.println("NullPointerException 例外をcatchしました");
+    //   System.out.println("ーースタックトレース（ここから）ーー");
+    //   e.printStackTrace();
+    //   System.out.println("ーースタックトレース（ここまで）ーー");
+    // }
+
+    // try {
+    // int i = Integer.parseInt("三");
+    // } catch (NumberFormatException e) {
+    //   System.out.println("NumberFormatExceptionの例外をcatchしました。");
+    // }
   }
 }
