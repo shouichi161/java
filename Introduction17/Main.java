67051fa298b0d9908ddca69d896dbc6e4ac51d12
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-15 10:07:40 +0000

    [add]スタックトレースを追加

--- Introduction17/Main.java
+++ Introduction17/Main.java
@@ -8,8 +8,15 @@ public class Main {
     } catch (IOException e) {
       System.out.println("エラーが発生しました。");
     }
-    
-    Person p = new Person();
-    p.setAge(-200);
+
+    String s = null;
+    try {
+    System.out.println(s.length());
+    } catch (NullPointerException e) {
+      System.out.println("NullPointerException 例外をcatchしました");
+      System.out.println("ーースタックトレース（ここから）ーー");
+      e.printStackTrace();
+      System.out.println("ーースタックトレース（ここまで）ーー");
+    }  
   }
 }
