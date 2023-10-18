d7831557af07336a90eb067c77da577df05c6e48
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-18 13:51:00 +0000

    [add]StringBuilderを追加

---
+++ practice1/1-4/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 100; i++) {
+      sb.append(i + " ");
+    }
+    String s = sb.toString();
+    System.out.println(s);
+    
+    
+  }
+}
