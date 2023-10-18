6c4de51177697e5680aea8b0d48f7d66b652db99
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-18 13:39:08 +0000

    [add]文字列変換メソッドを追加

--- practice1/1-3/Main.java
+++ practice1/1-3/Main.java
@@ -3,5 +3,13 @@ public class Main {
     String s1 = "Java programming";
     System.out.println("文字列s1の4文字目以降は" + s1.substring(3));
     System.out.println("文字列s1の4~8文字目は" + s1.substring(3,8));
+
+    String s2 = "JAVA PROGRAMMING";
+    System.out.println(s2.toLowerCase());
+    System.out.println(s1.toUpperCase());
+    System.out.println(s1.replace("Java", "Ruby"));
+    
+    String s3 = "   Java programming ";
+    System.out.println(s3.trim());
   }
 }
