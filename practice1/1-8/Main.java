eebc82f5bb6c7303278dafc55d2ee3909c696f9a
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-19 14:55:32 +0000

    [add]replaceAllメソッドを追加

---
+++ practice1/1-8/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    String s = "abc,def:ghi";
+    String w = s.replaceAll("[beh]","X");
+    System.out.println(w);
+    
+    String s2 = "こんにちは";
+    String w2 = s2.replaceAll("[こんにちは]","こんばんは");
+    System.out.println(w2);
+    String w3 = s2.replaceAll("こんにちは","こんばんは");
+    System.out.println(w3);
+  }
+}
