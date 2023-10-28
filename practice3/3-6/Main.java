5861d8f29285665543a43f463cc4bbdd94f1af27
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-28 11:39:45 +0000

    [add]LinkedHashSetを追加

--- practice3/3-6/Main.java
+++ practice3/3-6/Main.java
@@ -1,5 +1,6 @@
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.LinkedHashSet;
 
 public class Main {
   public static void main(String[] args) {
@@ -16,6 +17,21 @@ public class Main {
     words.add("林檎");
     for(String s : words) {
       System.out.print(s + "→");
+    }
+    
+    Set<String> words2 = new LinkedHashSet<String>();
+    words2.add("dog");
+    words2.add("cat");
+    words2.add("wolf");
+    words2.add("panda");
+    words2.add("りんご");
+    words2.add("みかん");
+    words2.add("もも");
+    words2.add("葡萄");
+    words2.add("桃");
+    words2.add("林檎");
+    for(String s2 : words2) {
+      System.out.print(s2 + "→");
     }
     
   }
