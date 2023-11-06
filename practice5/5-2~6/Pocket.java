996aeeafb6cfa6a84b514ff7ffd567152da1db36
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-11-06 14:39:14 +0000

    [add]ジェネリクスを追加

---
+++ practice5/5-2~6/Pocket.java
@@ -0,0 +1,17 @@
+public class Pocket<String> {
+  // private Object data;
+  // public void put(Object d) {
+  //   this.data = d;
+  // }
+  // public Object get() {
+  //   return this.data;
+  // }
+
+  private String data;
+  public void put(String d) {
+    this.data = d;
+  }
+  public String get() {
+    return this.data;
+  }
+}
