ee1670dbb76c2586311976cfc98f8904390e1930
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-11-14 11:45:34 +0000

    [add]6-3を追加

---
+++ practice6/6-3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static int sub(int a, int b) {
+    return a - b;
+  }
+  public static void main(String[] args) {
+    MyFunction func = Main::sub;
+    int a = func.call(5,3);
+    System.out.println("5-3は" + a);
+  }
+}
