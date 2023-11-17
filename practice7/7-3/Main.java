de0a298f5587554a1c3ca54a38d897c60a0fa3e3
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-11-17 13:49:17 +0000

    [add]7-3を追加

---
+++ practice7/7-3/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("ご利用のJavaのバージョン:");
+    System.out.println(System.getProperty("java.version"));
+    Properties p = System.getProperties();
+    Iterator<String> i = p.stringPropertyNames().iterator();
+    System.out.println("【システムプロパティ一覧】");
+    while(i.hasNext()) {
+      String key = i.next();
+      System.out.print(key + " = ");
+      System.out.println(System.getProperty(key));
+    }
+  }
+}
