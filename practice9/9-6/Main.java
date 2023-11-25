20d3e3457e7aa5fc883ef12209ab0b5a3d31730a
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-11-25 13:07:12 +0000

    [add]9-6を追加

---
+++ practice9/9-6/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    baos.write(65);
+    baos.write(66);
+    byte[] data = baos.toByteArray();
+  }
+}
